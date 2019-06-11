e2d121888dcc889939eec921586d3b0d8f9e25a9
seongjin571 <gkstjdwls11@naver.com>
2019-06-11 22:25:52 +0900

    oop js

---
+++ es6/oop.js
@@ -0,0 +1,22 @@
+const SYM = Symbol();
+const o = {a: 1, b: 2, c: 3, [SYM]: 4};
+for(let prop in o){
+    console.log(`${prop} : ${o[prop]}`);
+    console.log(prop);
+    if(o.a == o.prop){
+        console.log("a");
+    }
+}
+console.log(o.a);
+// console.log(o[a]);
+const array = [10,9,8,7];
+for(let i in array){
+    console.log(i);
+}
+array.forEach(e => console.log(e));
+Object.keys(o).forEach(p => console.log(`${p} : ${o[p]}`));
+
+const o2 = { x2: 1, x3: 2, f2: 4, xle: 5};
+Object.keys(o2).filter(e => e.match(/^x/)).forEach(v => {
+    console.log(`${v} : ${o2[v]}`);
+});
